27ff16e22ab1ff8caa4eac606e5470a33a7271cf
Mateusz Kępa <matt.kepa@gmail.com>
2023-02-25 20:32:21 +0100

    feat: add login and logout user functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request, Response, make_response, session, jsonify
 from flask_socketio import SocketIO
+from flask_cors import cross_origin
 import config
+from uuid import uuid4
 
 
 # SETUP APP
@@ -12,23 +14,69 @@ socketio = SocketIO(app)
 
 
 # ----------------------
+# APP CONSTANTS
+NAME_KEY = 'user'
+
+# APP VARIABLES
+users = []
+
+
+# ----------------------
 # VIEWS
 
 @app.route('/')
 @app.route('/home')
 def home():
     """
-    Displays home page of chat app
+    Displays home page of chat app if user is logged in
     """
+    if NAME_KEY not in session:
+        return redirect(url_for('login'))
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """
     Displays login page
     """
+    if request.method == 'POST':
+        # create and save new active user
+        user = { 'id': str(uuid4()), 'username': request.form['username'] }
+        users.append(user)
+        session[NAME_KEY] = user
+        return redirect(url_for('home'))
     return render_template('LoginPage.html')
+
+
+# ----------------------
+# API FUNCTIONS
+
+@app.route('/api/logout', methods=['POST'])
+def logout():
+    """
+    Handles logging the current user out by popping user from session
+    and removing from active users list
+    """
+    if request.method == 'POST':
+        usr = request.get_json()
+        usr_id = usr['uid']
+        for i, usr in enumerate(users):
+            if users[i]['id'] == usr_id:
+                del users[i]
+
+        session.pop(NAME_KEY, None)
+        return usr
+
+
+@app.route('/api/user')
+def get_user():
+    """
+    Called only from frontend axios to get current logged in user
+    """
+    if NAME_KEY in session:
+        return jsonify(session[NAME_KEY])
+    return {}
 
 
 
--- static/app.js
+++ static/app.js
@@ -2,13 +2,48 @@
 const toggleUsersBtn = document.querySelector('.toggle-users-btn');
 const hideUsersBtn = document.querySelector('.hide-users-btn');
 const usersSection = document.querySelector('.users-container');
+const logoutBtn = document.querySelector('.logout-btn');
+
+//
+//
+// Global Variable
+let currUser = null;
+
+//
+//
+// Fetch current user
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const response = await axios.get('/api/user');
+    currUser = response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 //
 //
 // Binding event listeners to buttons to interact with interface
 toggleUsersBtn.addEventListener('click', handleToggleUsers);
 hideUsersBtn.addEventListener('click', handleToggleUsers);
+logoutBtn.addEventListener('click', handleLogout);
 
 function handleToggleUsers(e) {
   usersSection.classList.toggle('active');
+}
+
+async function handleLogout(e) {
+  try {
+    console.log({ uid: currUser.id });
+    await axios({
+      method: 'post',
+      url: '/api/logout',
+      data: { uid: currUser.id },
+      headers: { 'Content-Type': 'application/json' },
+    });
+    currUser = null;
+    window.location.pathname = '/login';
+  } catch (error) {
+    console.log(error);
+  }
 }
